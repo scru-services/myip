1ffd929132dd07f23fb750cd9c86324f3fceb51e
Joel Stüdle <joel.stuedle@gmail.com>
2018-02-15 19:54:06 +0100

    use example setup to create project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# what-is-my-ip
+# What is my IP by Scru-Services
+
+Scru-Services offers you a bunch of free online tools crafted by developers to increase their coding skills.
+
+The _What is my IP_ service displays your public IP address by just visiting this page. In addition the service checks the local addressing if available. The Internet Protocol address [IP address](https://en.wikipedia.org/wiki/IP_address) is the numerical reference of your device in the network. The IP address serves two principal functions, host or network interface identification and location addressing.
+
+---------
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+		// include the domain information from the root dir
+		require '../../inc/domain.php';
+
+		// include the project_data from the project dir
+		// check this data if you setup a new project
+		require 'inc/project_data.php';
+
+		// include the header from the root dir
+		require '../../inc/header.php';
+
+?>
+
+	<!-- scru-service start -->
+
+	<?php
+
+		// get visitory ip address
+		$client  = @$_SERVER['HTTP_CLIENT_IP'];
+		$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+		$remote  = @$_SERVER['REMOTE_ADDR'];
+
+		if (filter_var($client, FILTER_VALIDATE_IP)){
+			$ip = $client;
+			} elseif (filter_var($forward, FILTER_VALIDATE_IP)){
+			$ip = $forward;
+			} elseif (filter_var($remote, FILTER_VALIDATE_IP)) {
+			$ip = $remote;
+			} else {
+			$ip = false;
+		}
+
+		// create result array and try to get data with ip from geoplugin
+		$result 	= array( 'ip' => $ip, 'ip_data' => false );
+		$result['ip_data'] = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $result['ip']));
+
+	?>
+
+	<?php if ($result['ip']): ?>
+
+		<div class="result-container success">
+			<h4>IP address</h4>
+			<div class="result" contenteditable="true" onclick="document.execCommand('selectAll',false,null)"><?php echo $result['ip']; ?></div>
+		</div><!-- .result.ip -->
+
+	<?php else: ?>
+
+		<div class="result-container error">
+			<h4>Error</h4>
+			<div class="result" contenteditable="true" onclick="document.execCommand('selectAll',false,null)">No IP address available</div>
+		</div><!-- .result.error -->
+
+	<?php endif; ?>
+
+	<?php if ($result['ip_data']->geoplugin_status == 200): ?>
+
+		<?php if ($result['ip_data']->geoplugin_city): ?>
+			<div class="result-container success">
+				<h4>IP origin city</h4>
+				<div class="result" contenteditable="true" onclick="document.execCommand('selectAll',false,null)"><?php echo $result['ip_data']->geoplugin_city; ?></div>
+			</div><!-- .result.city -->
+		<?php endif; ?>
+
+		<?php if ($result['ip_data']->geoplugin_countryName): ?>
+			<div class="result-container success">
+				<h4>IP origin country</h4>
+				<div class="result" contenteditable="true" onclick="document.execCommand('selectAll',false,null)"><?php echo $result['ip_data']->geoplugin_countryName; ?></div>
+			</div><!-- .result.country -->
+		<?php endif; ?>
+
+	<?php endif; ?>
+
+
+	<!-- scru-service end -->
+
+<?php
+
+	// include the domain information from the root dir
+	require '../../inc/footer.php';
+
+?>
